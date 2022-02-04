76b6758fe465310582068b8749bff99f19e06f43
DewsDavid31 <ddews1@msudenver.edu>
2022-02-04 16:06:37 -0700

    Added async method to testing to get drone, doesn't work yet

--- Drone.Tests/DroneTests.cs
+++ Drone.Tests/DroneTests.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
 using FlyingDrone;
 using FlyingPizza.Drone;
 using FlyingPizza.Services;
+using Microsoft.AspNetCore.Components;
 using Xunit;
 
 namespace Drone.Tests
