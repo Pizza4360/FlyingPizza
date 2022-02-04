3d3a8af85ffba846ce3e5eeaa2edd46dd7d51148
DewsDavid31 <ddews1@msudenver.edu>
2022-02-03 17:03:01 -0700

    Hotfix for yah frontenders

--- FlyingPizza/Services/RestDbSvc.cs
+++ FlyingPizza/Services/RestDbSvc.cs
@@ -30,7 +30,7 @@ namespace FlyingPizza.Services
             http.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(
                 "Basic",
                 Convert.ToBase64String(
-                    System.Text.Encoding.ASCII.GetBytes("admins:Snarfblat")));
+                    System.Text.Encoding.ASCII.GetBytes("admin:secret")));
 
 
             http.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
@@ -50,7 +50,7 @@ namespace FlyingPizza.Services
             http.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(
                 "Basic",
                 Convert.ToBase64String(
-                    System.Text.Encoding.ASCII.GetBytes("admins:Snarfblat")));
+                    System.Text.Encoding.ASCII.GetBytes("admin:secret")));
 
 
             http.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
@@ -70,7 +70,7 @@ namespace FlyingPizza.Services
                 client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(
                     "Basic",
                     Convert.ToBase64String(
-                        System.Text.Encoding.ASCII.GetBytes("admins:Snarfblat")));
+                        System.Text.Encoding.ASCII.GetBytes("admin:secret")));
 
 
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
@@ -137,7 +137,7 @@ namespace FlyingPizza.Services
             request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(
                 "Basic",
                 Convert.ToBase64String(
-                    System.Text.Encoding.ASCII.GetBytes("admins:Snarfblat")));
+                    System.Text.Encoding.ASCII.GetBytes("admin:secret")));
 
             return await http.SendAsync(request);
         }
