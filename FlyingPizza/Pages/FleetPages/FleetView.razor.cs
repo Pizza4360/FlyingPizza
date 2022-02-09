996b8e33e07560bc4d6cbb9aadbfbf2a45f66be6
dbowling21 <dbowlin2@msudenver.edu>
2022-02-09 12:12:59 -0700

    Connected Fleetview page to the mongo database. Drones displayed are now from the database and show status from it. Added a staus bar at top to show size of fleet.

--- FlyingPizza/Pages/FleetPages/FleetView.razor.cs
+++ FlyingPizza/Pages/FleetPages/FleetView.razor.cs
@@ -1,37 +1,30 @@
-﻿using FlyingDrone.Pages.FleetPages;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FlyingPizza.Drone;
+
 
 namespace FlyingPizza.Pages.FleetPages
 {
     partial class FleetView : ComponentBase
     {
 
-        public string Greeting = null;
         public string[] block;
+        public DroneModel[] Fleet = null;
+        public int size;
 
-        protected override void OnInitialized()
+        protected override async Task OnInitializedAsync()
+        {
+            Fleet = await restPoint.Get<DroneModel[]>("http://localhost:8080/Fleet/?sort={badgeNumber:1}");
+            size = Fleet.Length;
+
+        }
+       /* protected override void OnInitialized()
         {
             Greeting = "Hello";
             block = new string[5]{ "drone1", "drone2", "drone3", "drone4", "drone5" };
-
-            //DroneData[] drone = await restDbSvc.Get<DroneData[]>("localhost:4544/Fleet?filter={ }");
-            //DroneData drone = await restDvSvc.Post<DroneData>(url, DroneData)
-
-            // Post template
-            //Case newCase = new Case() { caseStatus = "uninitialized" };
-            //var rval = await restDbSvc.Post<Case>("http://localhost:8080/cases/", newCase);
-
-            //newCaseURL = rval.Headers.Location.AbsoluteUri;
-
-            //caseVar = await restDbSvc.Get<Case>(newCaseURL);
-            //caseVar.url = newCaseURL;
-
-        }
-
-
+        } */
     }
 }
