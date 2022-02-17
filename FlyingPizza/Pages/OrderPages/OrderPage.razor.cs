978c08c07991b0315158f01eb9010641bdc50b6e
KamronCorreia1 <kamron.w.correia@gmail.com>
2022-02-16 18:45:47 -0700

    adding some things to clarify order page and some small css tweaks till david bowling can fix it

--- FlyingPizza/Pages/OrderPages/OrderPage.razor.cs
+++ FlyingPizza/Pages/OrderPages/OrderPage.razor.cs
@@ -11,22 +11,22 @@ namespace FlyingPizza.Pages.OrderPages
     {
         Order custOrder;
 
-        protected override async Task OnInitializedAsync()
+        protected override void OnInitialized()
         {
             // Make a new order for the customer to use when the page boots up
             custOrder = new Order();
 
-            // send order to database empty
-            var r = await restpoint.Post<Order>("http://localhost:8080/Orders", custOrder);
-            custOrder.URL = r.Headers.Location.AbsoluteUri;
         }
 
         public async Task makeOrder()
         {
             // get time they ordered it
             custOrder.TimeOrdered = DateTime.Now.ToString();
+
             // upload final object to the server. 
-            var r = await restpoint.Put<Order>(custOrder.URL,custOrder);
+            var r = await restpoint.Post<Order>("http://localhost:8080/Orders", custOrder);
+            custOrder.URL = r.Headers.Location.AbsoluteUri;
+            r = await restpoint.Put<Order>(custOrder.URL,custOrder);
             // Navigate to page to display users current order. 
             // _navigationManager.NavigateTo("/userPage", false);
         }
