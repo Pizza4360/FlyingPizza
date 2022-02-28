3d1fee8529d0ff6f77651e052c3b7760289d2668
KamronCorreia1 <Kamron.w.correia@gmail.com>
2022-02-28 10:27:12 -0700

    Updating and reworking query gen to have one generate method that is based on which fields you input for it

--- FlyingPizza/Shared/QueryGen.cs
+++ FlyingPizza/Shared/QueryGen.cs
@@ -8,6 +8,47 @@ namespace FlyingPizza.Shared
     public class QueryGen
     {
 
+
+        public static string Generate(string collName, string arrayName = "", string filterType = "", string filterVal = "", string sort = "")
+        {
+
+            // basic get all other things except collection are blank
+            if (arrayName == "" && filterType == "" && filterVal == "" && sort == "")
+            {
+                connectionString = "http://localhost:8080/" + collName;
+            }
+            // collection and sort arent blank
+            else if(sort != "" && arrayName == "" && filterType == "" && filterVal == "")
+            {
+                connectionString = "http://localhost:8080/" + collName + "?sort={" + sort + "}";
+            }
+            // collection, filtertype, and filterval not blank
+            else if (filterType != "" && filterVal != "" && sort == "" && arrayName == "")
+            {
+                connectionString = "http://localhost:8080/" + collName + "?filter={'" + filterType + "':'" + filterVal + "'}";
+            }
+            // collection filter type, filterVal, and sort are not blank
+            else if (filterType != "" && filterVal != "" && sort != "" && arrayName == "")
+            {
+                connectionString = "http://localhost:8080/" + collName + "?filter={'" + filterType + "':'" + filterVal + "'}&sort={" + sort + "}";
+            }
+            // collection array name, filterType, filterVal
+            else if (filterType != "" && filterVal != "" && arrayName != "" && sort == "")
+            {
+                connectionString = "http://localhost:8080/" + collName + "?filter={'" + arrayName + "':{'" + filterType + "':'" + filterVal + "'}";
+            }
+            // collection, array name, filterType, FilterVal, sort are not blank
+            //else if (arrayName != "" && filterType != "" && filterVal != "" && sort != "")
+            //{
+            //    connectionString = "http://localhost:8080/" + collName + "?filter={'" + arrayName + "':{'" + filterType + "':'" + filterVal + "'}";
+            //}
+
+
+            return connectionString;
+
+
+        }
+
         // this class will primarily control the connection to the restheart server by generating filtered strings 
         // collName is the name of the table you want to connect to 
         // filtertype is the attribute you want to filter by for example badgenumber
@@ -15,42 +56,6 @@ namespace FlyingPizza.Shared
         // sort is the sort string and direction so 'badgeNumber':1 to get them in ascending order and 'badgenumber':-1 for descending
         public static string connectionString = null;
 
-         
-        // This method just grabs the whole collection
-        public static string GetColl(string collName)
-        {
-            connectionString = "http://localhost:8080/" + collName;
-            return connectionString;
-        }
-
-        // This method grabs the whole collection sorted
-        public static string GetCollSort(string collName, string sort)
-        {
-            connectionString = "http://localhost:8080/" + collName + "?sort={" + sort + "}";
-            return connectionString;
-        }
-
-        // This method takes in a collection name, filter type, and filter value and an optional sort value
-        // and returns a string which can be used by the RestDbSvc class for connections.
-        public static string BasicFilter(string collName, string filterType, string filterVal)
-        {
-            connectionString = "http://localhost:8080/" + collName + "?filter={'" + filterType + "':'" + filterVal + "'}";
-            return connectionString;
-        }
-        
-        // This is a basic filter but done sorted
-        public static string BasicFilterSort(string collName, string filterType, string filterVal, string sort)
-        {
-            connectionString = "http://localhost:8080/" + collName + "?filter={'" + filterType + "':'" + filterVal + "'}&sort={"+ sort +"}";
-            return connectionString;
-        }
-
-
-        public static string QueryArray(string collName, string arrayName, string filterType, string filterVal)
-        {
-            connectionString = "http://localhost:8080/" + collName + "?filter={'" + arrayName + "':{'"+filterType+"':'" + filterVal + "'}";
-            return connectionString;
-        }
 
 
     }
