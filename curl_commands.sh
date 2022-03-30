c804700badfb51e785b7bfea7d8e32328c2e1e5a
MXHJohn314 <mxhjohnson4dba@gmail.com>
2022-03-29 19:17:42 -0600

    Updated curl_commands.sh

--- curl_commands.sh
+++ curl_commands.sh
@@ -1,11 +1,21 @@
-# for containerizing a drone
-docker run -it --rm -p 5000:5000 -p 5001:5001 -e ASPNETCORE_HTTP_PORT=5000 -e ASPNETCORE_URLS=http://+:5001 --name drone drone
+##SETUP##
+# !!ONLY DO THIS ONCE!! for creating the docker network
+docker network create --subnet=172.18.0.0/16 pizza_drones 
 
-# for containerizing dispatcher
-docker run -it --rm -p 4000:4000 -e ASPNETCORE_HTTP_PORT=4000 -e ASPNETCORE_URLS=http://+:4000 --name dispatch dispatch
+# for building drone and dispatcher containers
+docker build -t dispatcher -f DroneDispatcher/Dockerfile . && docker build -t drone -f DroneSimulator/Dockerfile .
+
+# for running the dispatcher container
+docker run -it --rm -p 4000:4000 -e ASPNETCORE_HTTP_PORT=4000 -e ASPNETCORE_URLS=http://+:4000 --name dispatcher dispatcher --net pizza_drones --ip 172.18.0.0
+
+# for running a drone container
+docker run -it --rm -p 5000:5000 -p 5001:5001 -e ASPNETCORE_HTTP_PORT=5000 -e ASPNETCORE_URLS=http://+:5001 --name drone drone --net pizza_drones --ip 172.18.0.1
+
+##SENDING DELIVERY##
+curl -X POST localhost:4000/dispatcher/add_order -H 'Content-type: application/json' -H 'Accept: application/json' -d '{ "orderId": "TESTmrtyuafwdnrusdku"}'
 
 
-#REGISTRATION
+##TODO DRONE REGISTRATION##
 # when front end tells dispatcher to register a drone:
 curl -X POST localhost:4000/dispatcher/register \
 -H 'Content-type: application/json' \
@@ -25,13 +35,3 @@ curl -X POST localhost:5001/drone/initregistration \
      }, \
      "dispatcherUrl": "http://localhost:5000/dispatcher" \
  }'
-
-#DELIVERY
-# when front end tells dispatcher to deliver an order:
-curl -X POST localhost:4000/dispatcher/add_order \
--H 'Content-type: application/json' \
--H 'Accept: application/json' \
--d '{ "orderId": "TESTmrtyuafwdnrusdku"}'
-
-
-
