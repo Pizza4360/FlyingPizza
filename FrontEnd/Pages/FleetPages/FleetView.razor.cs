d60de01ad6a97e23749e7e2a9671074f50624e14
dbowling21 <dbowlin2@msudenver.edu>
2022-04-05 15:58:41 -0600

    Fixed the index.html to load all css files correctly and updated fleet pages to their most current version.

--- FrontEnd/Pages/FleetPages/FleetView.razor.cs
+++ FrontEnd/Pages/FleetPages/FleetView.razor.cs
@@ -10,17 +10,23 @@ namespace FlyingPizza.Pages.FleetPages
     {
         public DroneRecord[] Fleet = null;
         public int size;
+        public Boolean connection = false;
         protected override async Task OnInitializedAsync()
         {
-            Fleet = await restPoint.Get<DroneRecord[]>("http://localhost:8080/Fleet/?sort={badgeNumber:1}");
-            size = Fleet.Length;
+            try
+            {
+                Fleet = await restPoint.Get<DroneRecord[]>("http://localhost:8080/Fleet/?sort={badgeNumber:1}");
+                size = Fleet.Length;
+                connection = true;
+            }
+            catch { 
+            }         
         }
 
-        public async Task<Boolean> GoToDrone(DroneRecord drone)
+        public async Task GoToDrone(DroneRecord drone)
         {
             globalData.currDrone = drone;
-            var r = await dialogService.OpenAsync<DetailedDrone>("View Drone");
-            return r;
+            await dialogService.OpenAsync<DetailedDrone>("View Drone");           
         }
       
     }
