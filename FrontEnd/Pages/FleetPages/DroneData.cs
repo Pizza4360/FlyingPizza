277d0031d803d94b3e6a823b418cd09e64f99ab2
MXHJohn314 <mxhjohnson4dba@gmail.com>
2022-04-05 13:25:22 -0600

    got .NET 6 working for front end.

---
+++ FrontEnd/Pages/FleetPages/DroneData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FlyingPizza.Drone;
+
+namespace FlyingDrone.Pages.FleetPages
+{
+    public class DroneData
+    {
+
+        public string DroneID { get; set; }
+        public string Status { get; set; }
+        public string Url { get; set; }
+        public string Order { get; set; }
+        public string batteryLife { get; set; }
+        public Point location { get;set;}
+        public Point destination { get; set; }
+
+    }
+}
