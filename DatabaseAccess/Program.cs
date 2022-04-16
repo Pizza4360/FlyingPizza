bf91595763eed084ccbce8667ef181fca447edca
MXHJohn314 <mxhjohnson4dba@gmail.com>
2022-04-15 20:57:31 -0600

    ådded DatabaseAccess project

---
+++ DatabaseAccess/Program.cs
@@ -0,0 +1,42 @@
+using DatabaseAccess.Repositories;
+using Domain.InterfaceDefinitions.Gateways;
+using Domain.InterfaceImplementations.Gateways;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+#region repositories
+
+// builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("OrdersDb"));
+// builder.Services.AddSingleton<OrderRepository>();
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("FleetDb"));
+builder.Services.AddSingleton<FleetRepository>();
+
+builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+builder.Services.AddScoped<IDispatchToDroneGateway>(_ => new DispatchToDroneGateway());
+#endregion repositories
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
