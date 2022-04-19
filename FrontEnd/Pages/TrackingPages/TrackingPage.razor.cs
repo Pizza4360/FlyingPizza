a99ccd0fc3dbfbce614881f4b7848657b9178522
MXHJohn314 <mxhjohnson4dba@gmail.com>
2022-04-19 13:52:22 -0600

    moved marker info to code behind in MarkerData.

--- FrontEnd/Pages/TrackingPages/TrackingPage.razor.cs
+++ FrontEnd/Pages/TrackingPages/TrackingPage.razor.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Domain.Entities;
 using FrontEnd.Services;
 using Microsoft.AspNetCore.Components;
 using Radzen;
+using Radzen.Blazor;
 
 
 namespace FrontEnd.Pages.TrackingPages;
@@ -14,7 +16,7 @@ partial class TrackingPage : ComponentBase
     public static IEnumerable<DroneRecord> DroneRecords;
     private const int RefreshInterval = 2000;
     private Timer _timer;
-    public GoogleMapPosition markers;
+    public IEnumerable<RadzenGoogleMapMarker> MarkerData;
 
     protected override Task OnInitializedAsync()
     {
@@ -32,9 +34,26 @@ partial class TrackingPage : ComponentBase
     {
         try
         {
-            DroneRecords = await HttpMethods.Get<List<DroneRecord>>("http://localhost:5127/DatabaseAccess/GetFleet");
-            await InvokeAsync(StateHasChanged);
+            MarkerData = (await HttpMethods.Get<List<DroneRecord>>("http://localhost:5127/DatabaseAccess/GetFleet"))
+                .Select(
+                    (drone, i) => new RadzenGoogleMapMarker
+                    {
+                      // Id=@(drone.BadgeNumber) 
+                        Title="Drone #" + i,
+                        Label="Drone #" + i++,
+                        Position=new GoogleMapPosition {
+                            Lat = (double) drone.CurrentLocation.Latitude, 
+                            Lng = (double) drone.CurrentLocation.Longitude 
+                        }
+                    });
+            
+            // await InvokeAsync(StateHasChanged);
         }
         catch{}
+    }
+
+    public class OurMap : RadzenGoogleMap
+    {
+        
     }
 }
