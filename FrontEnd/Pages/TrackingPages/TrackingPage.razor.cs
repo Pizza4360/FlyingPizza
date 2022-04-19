1196bc5b943d39cd8cf488762236982f2584a58c
MXHJohn314 <mxhjohnson4dba@gmail.com>
2022-04-19 08:25:17 -0600

    Added logic for callback for a two second timer to refresh the page. Needs to be refactored to only update the markers instead of the entire page, to avoid zooming out every two seconds when re-rendering.

--- FrontEnd/Pages/TrackingPages/TrackingPage.razor.cs
+++ FrontEnd/Pages/TrackingPages/TrackingPage.razor.cs
@@ -1,25 +1,39 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Domain.Entities;
 using FrontEnd.Services;
 using Microsoft.AspNetCore.Components;
 using Radzen;
-using Radzen.Blazor;
 
 
 namespace FrontEnd.Pages.TrackingPages;
 
 partial class TrackingPage : ComponentBase
 {
-    public IEnumerable<DroneRecord> DroneRecords;
+    public static IEnumerable<DroneRecord> DroneRecords;
+    private const int RefreshInterval = 2000;
+    private Timer _timer;
+    public GoogleMapPosition markers;
 
-    protected override async Task OnInitializedAsync()
+    protected override Task OnInitializedAsync()
+    {
+        _timer = new Timer(
+            Callback,
+            null, 
+            0,
+            RefreshInterval);
+        return Task.CompletedTask;
+    }
+
+    private async void Callback(object _) => await UpdateDroneMarkers();
+
+    private async Task UpdateDroneMarkers()
     {
         try
         {
-            DroneRecords = await HttpMethods.Get<List<DroneRecord>>(
-                "http://localhost:5127/DatabaseAccess/GetFleet");
+            DroneRecords = await HttpMethods.Get<List<DroneRecord>>("http://localhost:5127/DatabaseAccess/GetFleet");
+            await InvokeAsync(StateHasChanged);
         }
         catch{}
     }
