45c767fa6c895df5f3415b43495485a5daf50e63
hdelgad3 <hdelgado9@outlook.com>
2022-04-29 22:45:19 -0600

    Added Drone url input field and added env var for api key

---
+++ FrontEnd/Services/ConvertAddressToGeoLocation.cs
@@ -0,0 +1,36 @@
+﻿
+
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Domain.DTO;
+
+namespace FrontEnd.Services
+{
+    public class ConvertAddressToGeoLocation
+    {
+        public string ApiKey { get; set; }
+        private const string _urlStart = "https://maps.googleapis.com/maps/api/";
+        private HttpClient httpClient = new HttpClient();
+
+         public ConvertAddressToGeoLocation() {
+            ApiKey = Environment.GetEnvironmentVariable("ApiKey");
+         }
+
+
+        public async Task<GeoLocation> CoordsFromAddress(string address)
+        {
+            var url = _urlStart + "geocode/json?address=" + System.Web.HttpUtility.UrlEncode(address) + ",&key=" + ApiKey;
+            var req = await httpClient.GetStringAsync(url);
+
+            Console.Write("RETURNED FROM RED: " + req);
+      
+            var js = JObject.Parse(req);
+            var lng = decimal.Parse(Convert.ToString(js["results"]?[0]?["geometry"]?["location"]?["lng"]) ?? string.Empty);
+            var lat = decimal.Parse(Convert.ToString(js["results"]?[0]?["geometry"]?["location"]?["lat"]) ?? string.Empty);
+            return new GeoLocation { Latitude = lat, Longitude = lng};
+        }
+
+    }
+}
