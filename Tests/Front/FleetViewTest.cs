c90ebb775e1fd2ae55d3888a511c50fe492386ff
hdelgad3 <hdelgado9@outlook.com>
2022-05-08 16:29:13 -0600

    bunit tests work but only if the database string is hardcoded. Couldnt figure out how to use env vars with bunit. also did tests in selenium.

--- Tests/Front/FleetViewTest.cs
+++ Tests/Front/FleetViewTest.cs
@@ -1,6 +1,16 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.Linq;
+using AngleSharp.Text;
+using Bunit;
+using FrontEnd.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Xunit;
 using TestContext = Bunit.TestContext;
+using Radzen;
+using Xunit.Abstractions;
+using Assert = Xunit.Assert;
+
 
 namespace Tests.Front;
 
@@ -11,10 +21,31 @@ public class FleetViewTest
     public static void TestFleetDrone()
     {
         using var cxt = new TestContext();
-        //var check = cxt.RenderComponent<FrontEnd.Pages.FleetPages.FleetView>();
-        //Xunit.Assert.Equal("Fleet Size: 2", check.Find($"b").TextContent);
+        cxt.Services.AddSingleton<DialogService>();
+        cxt.Services.AddSingleton<GlobalDataSvc>();
+        // cxt.Services.AddScoped(_=> new FrontEndToDatabaseGateway("DatabaseAccess"));
+        // var check = cxt.RenderComponent<FrontEnd.Pages.FleetPages.FleetView>();
+        //
+        // var connectionBar = check.Find(".quick-look").LastChild.ToMarkup();
+        // Assert.Equal(connectionBar,
+        //     "\r\n<div class=\"connection-bar\" data-hover=\"Database: Connected\" b-j4hm45p6x7=\"\">\r\n  " +
+        //     "<span class=\"oi oi-circle-check\" style=\"color: #08af08\" b-j4hm45p6x7=\"\"></span>\r\n</div>" );
+        //
+        // var droneCountString = check.Find("b").TextContent;
+        // var droneCount = droneCountString.Split().Last();
+        // var droneObjCount = check.Find(".drone-fleet").Children.Length;
+        // Assert.Equal(int.Parse(droneCount), droneObjCount);
+        //
+        // var droneBtn = check.FindAll(".rz-button rz-button-md btn-primary rz-button-icon-only");
+        // foreach (var drone in droneBtn)
+        // {
+        //     drone.Click();
+        //     var detail = check.Find(".rz-dialog-title").TextContent;
+        //     Assert.Equal(detail, "DetailedSimDrone");
+        //     var closeBtn = check.Find("#ADASDADASD");
+        //     closeBtn.Click();
+        // }
 
-        //check.Find($".rz-button").Click();
-        //check.Find("b").MarkupMatches("<b>DroneId:Currentlocation:Domain.DTO.Shared.GeoLocationDestination: Domain.DTO.Shared.GeoLocationStatus: Charging</b>");
+
     }
 }
--- Tests/Front/main.py
+++ Tests/Front/main.py
@@ -2,8 +2,21 @@ import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+def check_fleet_view(driver):
+    page1 = driver.find_element(By.ID, "page1")
+    page1.click()
+
+    first_drone = driver.find_element(By.CLASS_NAME, "rz-button rz-button-md btn-primary rz-button-icon-only")
+    first_drone.click()
+
+    exit_button = driver.find_element(By.ID, "ADASDADASD")
+    exit_button.click()
+
+    data_connection = driver.find_element(By.CLASS_NAME, "connection-bar")
+    data_connection.click()
+
 def make_order(driver):
-    page2 = driver.find_element(By.ID, "page_2")
+    page2 = driver.find_element(By.ID, "page2")
 
     page2.click()
     customer_input = driver.find_element(By.ID, "CUST_NAME")
@@ -12,39 +25,72 @@ def make_order(driver):
     address_input = driver.find_element(By.ID, "ADDRESS")
     address_input.send_keys("1640 W Colfax Ave, Denver, CO 80204")
 
-    order_submit = driver.find_element(By.ID, "ORDER_SUBMIT")
+    order_submit = driver.find_element(By.ID, "ORDER_CREATE_BTN")
     order_submit.click()
 
+    order_id_input = driver.find_element(By.ID, "CANCEL_ORDER")
+    order_id_input.send_keys("1")
+    cancel_btn = driver.find_element(By.ID, "CANCEL_ORDER_BTN")
+    cancel_btn.click()
+
+    orders = driver.find_element(By.ID, "DRONE_FLEET_CARD")
+    print(orders)
+
+    drone_url = driver.find_element(By.ID, "DRONE_URL")
+    drone_url.send_keys("test_drone2")
+    add_drone = driver.find_element(By.ID, "ADD_DRONE_BTN")
+    add_drone.click()
+    drone_url2 = driver.find_element(By.ID, "DRONE_URL2")
+    drone_url2.send_keys("1")
+    remove_drone = driver.find_element(By.ID, "REMOVE_DRONE_URL")
+    remove_drone.click()
+
+
+
+
 def check_tracking(driver):
-    page3 = driver.find_element(By.ID, "page_3")
+    page3 = driver.find_element(By.ID, "page3")
     page3.click()
 
-    home_btn = driver.find_element(By.ID, "home_btn")
-    home_btn.click()
-    drone_btn = driver.find_element(By.ID, "drone_btn")
-    drone_btn.click()
-    dest_btn = driver.find_element(By.ID, "dest_btn")
-    dest_btn.click()
-    home_icon = driver.find_element(By.ID, "home_icon")
-    home_icon.click()
+    dropdown = driver.find_element(By.CLASS_NAME, "dropdown")
+    dropdown.click()
 
-    clear_btn = driver.find_element(By.CLASS_NAME, "mt-4")
-    clear_btn.click()
+    ready_btn = driver.find_element(By.XPATH, "//btn[text()='Ready']")
+    ready_btn.click()
+    dropdown.click()
+    delivering_btn = driver.find_element(By.XPATH, "//btn[text()='Delivering']").click()
+    dropdown.click()
+    returning_btn = driver.find_element(By.XPATH, "//btn[text()='Returning']").click()
+    dropdown.click()
+    dead_btn = driver.find_element(By.XPATH, "//btn[text()='Dead']").click()
+    dropdown.click()
+    charging_btn = driver.find_element(By.XPATH, "//btn[text()='Charging']").click()
+    dropdown.click()
+
+    sat_view = driver.find_element(By.XPATH, "//button[@title='Show satellite imagery']").click()
+    street_view = driver.find_element(By.XPATH, "//button[@title='Show street map']").click()
+
+
+
+
+
+    zoom_in = driver.find_element(By.XPATH, "//button[@title='Zoom in']")
+    zoom_in.click()
+
+    zoom_out = driver.find_element(By.XPATH, "//button[@title='Zoom out']")
+    zoom_out.click()
 
 def main():
     os.environ['PATH'] += r"C:\Users\hdelg\OneDrive\Desktop\Capstone\proj_tests\chromedriver.exe"
     driver = webdriver.Chrome()
-    driver.get('https://localhost:44364/')
+    driver.get('http://localhost:8081/')
     driver.implicitly_wait(30)
-    drone_conn_btn = driver.find_element(By.XPATH, "//div[@class='connection-bar']")
-    drone_conn_btn.click()
 
     # Checks the order put then the tracking page.
     make_order(driver)
     check_tracking(driver)
+    check_fleet_view(driver)
 
-    page1 = driver.find_element(By.ID, "page_1")
-    page1.click()
 
 if __name__ == "__main__":
     main()
