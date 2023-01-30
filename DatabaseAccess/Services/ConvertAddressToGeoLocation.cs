edb1b1a990995b303003aa7cf7c74e5c57a0fa51
MXHJohn314 <48652354+MXHJohn314@users.noreply.github.com>
2023-01-29 21:13:21 -0500

    Added a comment and renamed variables for readablity

--- DatabaseAccess/Services/ConvertAddressToGeoLocation.cs
+++ DatabaseAccess/Services/ConvertAddressToGeoLocation.cs
@@ -4,20 +4,22 @@ using Newtonsoft.Json.Linq;
 
 namespace DatabaseAccess.Services;
 
+
 public static class LocationParser
 {
-    private const string UrlStart = "https://maps.googleapis.com/maps/api/";
+    private const string BaseUrl = "https://maps.googleapis.com/maps/api";
     private static readonly HttpClient HttpClient = new();
 
-    public static async Task<GeoLocation> Parse(string apiKey, string address)
+    // Use the api key and address to form a full google maps location request url.
+    public static async Task<GeoLocation> Parse(string apiKey, string endpoint)
     {
-        Console.WriteLine($"\n\n\n\nParsing address from response:{address}\n\n\n\n");
-        var url = UrlStart + "geocode/json?address=" + HttpUtility.UrlEncode(address) + ",&key=" + apiKey;
-        var req = await HttpClient.GetStringAsync(url);
-        Console.Write("RETURNED: " + req);
-        var js = JObject.Parse(req);
-        var lng = decimal.Parse(Convert.ToString(js["results"]?[0]?["geometry"]?["location"]?["lng"]) ?? string.Empty);
-        var lat = decimal.Parse(Convert.ToString(js["results"]?[0]?["geometry"]?["location"]?["lat"]) ?? string.Empty);
-        return new GeoLocation {Latitude = lat, Longitude = lng};
+        Console.WriteLine($"\n\n\n\nParsing address from response:{endpoint}\n\n\n\n");
+        var requestUrl = $"{BaseUrl}/geocode/json?address={HttpUtility.UrlEncode(endpoint)},&key={apiKey};
+        var response = await HttpClient.GetStringAsync(requestUrl);
+        Console.Write("RETURNED: " + response);
+        var js = JObject.Parse(response);
+        var Longitude = decimal.Parse(Convert.ToString(js["results"]?[0]?["geometry"]?["location"]?["lng"]) ?? string.Empty);
+        var Latitude = decimal.Parse(Convert.ToString(js["results"]?[0]?["geometry"]?["location"]?["lat"]) ?? string.Empty);
+        return new GeoLocation {Latitude, Longitude};
     }
 }
